d4210f0db6f2cf454f8d9e3dfc6b541411fb4226
Miklos Torbics <torbics.miklos@gmail.com>
2021-06-28 12:24:51 +0200

    Add isEmpty and getScore helper functions

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,5 +1,19 @@
 const compareNumbers = (key) => (a, b) => a[key] - b[key];
 
+const isEmpty = (value) => value === null || value === undefined || value.length === 0;
+
+const getScore = (scoreboard) => {
+  if (scoreboard.scr) {
+    return scoreboard.scr;
+  } else if (!isEmpty(scoreboard.scrA) && !isEmpty(scoreboard.scrB)) {
+    return `${scoreboard.scrA}-${scoreboard.scrB}`;
+  } else {
+    return null;
+  }
+};
+
 module.exports = {
   compareNumbers,
+  isEmpty,
+  getScore,
 };
