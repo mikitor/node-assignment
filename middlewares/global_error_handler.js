660a2f91bb7790265c2b14478e29c508170d7ea1
Miklos Torbics <torbics.miklos@gmail.com>
2021-06-29 16:05:07 +0200

    Log if an unknown error has happened

--- middlewares/global_error_handler.js
+++ middlewares/global_error_handler.js
@@ -1,4 +1,11 @@
+const BadRequestError = require('../errors/bad_request');
+const NotFoundError = require('../errors/not_found');
+
 const globalErrorHandler = (error, req, res, next) => {
+  if (!(error instanceof BadRequestError || error instanceof NotFoundError)) {
+    console.error(`An unexpected error has occurred: ${error}`);
+  }
+
   const status = error.status || 500;
   res.status(status).json({
     success: false,
